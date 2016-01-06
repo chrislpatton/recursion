a4471b02fec3f7b1e57328ffd96bc904bbc6d0f2
Christopher Patton <chrislpatton@gmail.com>
2016-01-06 14:44:37 -0500

    implemented stringifyJSON.js, passed tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,49 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-};
+  	if (typeof obj ==='function'){
+  		return null;
+  	}
+
+  	if (typeof obj === 'undefined') {
+  		return null;
+  	}
+
+  	if (typeof obj === 'string') {
+  		return '"' + obj + '"';
+  	}
+
+  	if (typeof obj === 'number') {
+  		return String(obj);
+  	}
+
+  	if (obj === null) {
+  		return String(obj);
+  	}
+
+  	if (typeof obj === 'boolean') {
+  		return String(obj);
+  	}
+
+  	if (Array.isArray(obj)) {
+  		var arrContainer = [];
+  		for (var i = 0; i < obj.length; i++) {
+  			arrContainer.push(stringifyJSON(obj[i]));
+  		}
+  		return '[' + arrContainer + ']';
+  	}
+
+  	if (obj instanceof Object) {
+  		if (Object.keys(obj).length < 1) {
+  			return "{}";
+  		}
+  		var objContainer = [];
+		for (var key in obj) {
+  			if (stringifyJSON(obj[key]) !== null) {
+  				objContainer.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));  			
+  			}
+  		}
+  		
+  		return '{' + objContainer + '}';
+  	}
+  };
